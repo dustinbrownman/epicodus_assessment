fd8351cdf5aca9ad4a2547688172e26a63e66b68
Braden O'Guinn <nedarb906@gmail.com>
2013-10-30 11:33:23 -0700

    finish fizzbuzz

---
+++ html_css_jquery/fizzbuzz/js/scripts.js
@@ -0,0 +1,25 @@
+function fizzbuzz(number) {
+  if (number % 15 == 0) {
+    return 'fizzbuzz'
+  } else if (number % 3 == 0) {
+    return 'fizz'
+  } else if(number % 5 == 0) {
+    return 'buzz'
+  } else {
+    numbers = [];
+    for(var i = 0; i <= number; i++) {
+      numbers.push(i);
+    };
+    return numbers;
+  }
+};
+
+$(function() {
+  $('form').submit(function() {
+    // debugger;
+    buzzMessage = fizzbuzz($('input#number').val());
+
+    $('#status p').empty().append(buzzMessage).removeClass().addClass('success');
+    return false;
+  });
+});
--- html_css_jquery/fizzbuzz/spec/specs.js
+++ html_css_jquery/fizzbuzz/spec/specs.js
@@ -1,2 +1,21 @@
-//Place your specs here!
+describe('FizzBuzz', function() {
+  it('returns an array of all non-negative numbers up to the number given', function() {
+    fizzbuzz(4).should.eql([0, 1, 2, 3, 4]);
+  });
 
+  it('returns an empty array if the number is less than zero', function() {
+    fizzbuzz(-2).should.eql([]);
+  });
+
+  it('returns \'fizz\' if the number is evenly divisible by 3', function() {
+    fizzbuzz(6).should.equal('fizz');
+  });
+
+  it('returns \'buzz\' if the number is evenly divisible by 5', function() {
+    fizzbuzz(10).should.equal('buzz');
+  });
+
+  it('returns \'fizzbuzz\' if the number is evenly divisible by 15', function() {
+    fizzbuzz(15).should.equal('fizzbuzz');
+  });
+});
